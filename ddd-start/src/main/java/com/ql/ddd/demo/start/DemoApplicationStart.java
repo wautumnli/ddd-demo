8ceaebdba0b2c06186ec6a77e7f17cfb7d43101e
wanqiuli <wautumnli@163.com>
2022-02-16 20:12:30 +0800

    【fix】代码结构修改，gradle重新引入

--- ddd-start/src/main/java/com/ql/ddd/demo/start/DemoApplicationStart.java
+++ ddd-start/src/main/java/com/ql/ddd/demo/start/DemoApplicationStart.java
@@ -1,15 +1,15 @@
-package com.ql.ddd.start;
-
+package com.ql.ddd.demo.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author wanqiuli
+ * @date 2022/2/16 8:08 PM
  */
 @SpringBootApplication
-public class StartApplication {
+public class DemoApplicationStart {
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+        SpringApplication.run(DemoApplicationStart.class, args);
     }
 }
